79c4cdc0246743510e01d9fc40edc082ce7d5eac
tatyanepgoncalves <tatyane.2324165@aluno.faculdadedescomplica.com.br>
2025-02-08 10:41:54 -0300

    :sparkles: feat: update Main,py

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ else:
   print("Você ainda não pode votar!")
 
 # Missão 3: Recuperando o sistema de notas
+
 nota_aluno = input("Qual foi sua nota: ")
 
 if int(nota_aluno) >= 90 and int(nota_aluno) <= 100:
@@ -32,6 +33,7 @@ else:
   print("Estude um pouco mais, você tirou F")
 
 # Missão 4: Restaurando a Identificação de Números 
+
 numero_um = input("Digite um número: ")
 numero_dois = input("Digite outro número: ")
 
@@ -46,3 +48,43 @@ if senha_nova == "Python123":
   print("Cofre da biblioteca aberto.")
 else:
   print("Senha incorreta.")
+
+# Missão 6: Reforçando a Segurança e a Contagem do Sistema
+
+contador = 1
+
+while contador < 11:
+  print(contador)
+  contador += 1
+
+# Missão 7: Organizando a Lista
+
+lista_numeros = [8, 3, 10, 1, 5]
+lista_numeros.sort()
+print(lista_numeros)
+
+# Missão 8: Acessando os Registros de Alunos
+
+nome_aluno = ["Ana", "Bruno", "Carla", "Daniel", "Eduardo"]
+print(nome_aluno[0])
+print(nome_aluno[-1])
+
+# Missão 9: Calculando Dobro de um Número
+
+numero = input("Digite um número: ")
+
+soma = int(numero) + int(numero)
+
+print(f'O dobro de {numero} é', soma)
+
+# Missão 10: Contando Letras
+def contar_letras(nome):
+  contador = 0
+  for letra in nome:
+    if letra.isalpha():
+      contador += 1
+  return contador
+
+nome = input("Digite um nome: ")
+quantidade_letras = contar_letras(nome)
+print(f'O nome {nome} tem {quantidade_letras} letras.')
